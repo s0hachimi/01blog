3d9d6529298288dd4ffd494c35ad771798a795b9
sohachimi <hachimisoufain11@gmail.com>
2025-11-02 18:15:41 +0100

    init

--- backend/src/main/java/com/_blog/backend/DemoApplication.java
+++ backend/src/main/java/com/_blog/backend/DemoApplication.java
@@ -1,8 +1,6 @@
 package com._blog.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -11,8 +9,5 @@ public class DemoApplication {
     public static void main(String[] args) {
       SpringApplication.run(DemoApplication.class, args);
     }
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Hello %s!", name);
-    }
+  
 }
